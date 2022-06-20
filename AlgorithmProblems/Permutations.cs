6fab5992e47a4599c39e05371850d8ad22963c2c
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-06-20 22:34:04 +0530

    [vijayalakshmi] permutations of a given string

---
+++ AlgorithmProblems/Permutations.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProblems
+{
+    internal class Permutations
+    {
+
+        public void Permute(String s, String answer)
+        {
+            if (s.Length == 0)
+            {
+                Console.Write(answer + "  ");
+                return;
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char ap = s[i];
+                String left_substr = s.Substring(0, i);
+                String right_substr = s.Substring(i + 1);
+                String result = left_substr + right_substr;
+                Permute(result, answer + ap);
+            }
+        }
+    }
+}
