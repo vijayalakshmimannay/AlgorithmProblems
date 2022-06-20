f4c11e5b0d90792b5ea55e26b4479632883f4055
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-06-20 22:48:46 +0530

    [vijayalakshmi] bubble sort

--- AlgorithmProblems/Program.cs
+++ AlgorithmProblems/Program.cs
@@ -4,25 +4,18 @@ namespace AlgorithmProblems
 {
     class program
     {
-
-        public static void Main(String[] args)
+        static void Main(String[] args)
         {
 
-            string[] names = { "vijaya lakshmi", "lakshmi vijaya", "harsha talluri", "talluri harsha" };
-            AlgorithmProblems.InsertSort value = new AlgorithmProblems.InsertSort();
-            value.InsertionSort(names);
-            foreach (var item in names)
-            {
-                Console.WriteLine(item);
-            }
-
-
-
+            AlgorithmProblems.BubbleSort value = new AlgorithmProblems.BubbleSort();
+            value.BubbleSorting();
+            Console.Read();
         }
-
-
     }
 
 
 
 }
+
+
+
